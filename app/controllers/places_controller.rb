09a439042dcd1bdf187d12da0a3d4c3dfba9cf7e
toniahurst <antonia.a.hurst@gmail.com>
2019-07-03 03:17:31 +0000

    added place edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,9 +18,13 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-end 
+end
