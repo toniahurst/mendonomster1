77df100f02bd8a6c8f455f85049d5613f6f56234
toniahurst <antonia.a.hurst@gmail.com>
2019-07-03 00:20:09 +0000

    added place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,10 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
 
-	def index
-		@places = Place.all
-	end
+  def new
+  	@place = Place.new
+  end
 
 end
