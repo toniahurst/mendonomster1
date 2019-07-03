39b578ff48d89dd9ac3805c996dc0563f0b92493
toniahurst <antonia.a.hurst@gmail.com>
2019-07-03 04:57:31 +0000

    sigh

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all
@@ -9,29 +9,50 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-  end
+	def create
+	  @place = current_user.places.create(place_params)
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :new, status: :unprocessable_entity
+	  end
+	end
 
   def show
     @place = Place.find(params[:id])
   end
 
-  def edit
-    @place = Place.find(params[:id])
-  end
+	def edit
+	  @place = Place.find(params[:id])
+
+	  if @place.user != current_user
+	    return render plain: 'Not Allowed', status: :forbidden
+	  end
+	end
 
 	def update
 	  @place = Place.find(params[:id])
+
+	  if @place.user != current_user
+	    return render plain: 'Not Allowed', status: :forbidden
+	  end
+
 	  @place.update_attributes(place_params)
-	  redirect_to root_path
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :edit, status: :unprocessable_entity
+	  end
 	end
 
 	def destroy
-  	@place = Place.find(params[:id])
-  	@place.destroy
-  	redirect_to root_path
+	  @place = Place.find(params[:id])
+	  if @place.user != current_user
+	    return render plain: 'Not Allowed', status: :forbidden
+	  end
+
+	  @place.destroy
+	  redirect_to root_path
 	end
   
   private
