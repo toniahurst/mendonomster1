4d7b5887ca7096ea8ad05b1c2dd2cd1944f47078
toniahurst <antonia.a.hurst@gmail.com>
2019-07-03 00:24:44 +0000

    added form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,19 @@ class PlacesController < ApplicationController
   end
 
   def new
-  	@place = Place.new
+    @place = Place.new
+  end
+
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 
 end
