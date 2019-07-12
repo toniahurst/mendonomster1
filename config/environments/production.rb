49aabae58af52f1e371305db5a493be91c817ace
toniahurst <antonia.a.hurst@gmail.com>
2019-07-12 04:41:35 +0000

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,13 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'mendonomster1-tonia-hurst.herokuapp.com' }
+   ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
