83b7b43e2f09ee49c5be98a66a246aba7a48e84b
toniahurst <antonia.a.hurst@gmail.com>
2019-07-16 03:41:15 +0000

    ...and more tweaks

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-  	@places = Place.order("name").page(params[:page]).per(4)
+  	@places = Place.order("name").page(params[:page]).per(6)
   end
 
   def new
